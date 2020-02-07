a917bc49b67c0d1d725c977e7356f51b1bb6d07d
Aaron Timbol <timbolaj@gmail.com>
2020-02-07 20:12:08 +0000

    packing list

---
+++ sublime.js
@@ -0,0 +1,13 @@
+let number = 100;
+while (number <= 200) {
+  if (number % 3 === 0 && number % 4 === 0) {
+    console.log('LoopyLighthouse'); 
+  } else if (number % 4 === 0) {
+    console.log('Lighthouse');
+  } else if (number % 3 === 0) {
+    console.log('Loopy');
+  } else {
+    console.log(number);
+  }
+  number ++
+}
